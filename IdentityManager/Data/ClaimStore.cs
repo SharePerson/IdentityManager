b55aee992c5fe8d9654a522bfb2ad4c3db198d91
Fady Soliman <fady.soliman@sitereq.com>
2021-09-15 21:17:22 +0200

    claims store

---
+++ IdentityManager/Data/ClaimStore.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace IdentityManager.Data
+{
+    public static class ClaimStore
+    {
+        public static List<Claim> claimsList = new()
+        {
+            new ("Create", "Create"),
+            new ("Edit", "Edit"),
+            new ("Delete", "Delete")
+        };
+    }
+}
